d07de6b6e854d7c28dcb9099e78cbc680fa3a5ba
Jan Barášek <janbarasek@gmail.com>
2024-06-08 21:15:18 +0200

    AR: Fix CSS

--- src/pages/ARViewer.tsx
+++ src/pages/ARViewer.tsx
@@ -1,9 +1,9 @@
 import { resolveConnectionType } from '../core/connection/resolveConnectionType';
-import { Button, Box, Drawer } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import { IndicatorInfoList } from '../ui/IndicatorInfoList';
 import { useSearchParams } from 'react-router-dom';
 import { StatusBanner } from '../ui/StatusBanner';
-import { Footer } from '../components/Footer';
+import { Drawer } from '../ui/Drawer';
 import { useAR } from '../hooks/useAR';
 import { FC } from 'react';
 
@@ -18,41 +18,39 @@ export const ARViewer: FC = () => {
       <StatusBanner status="ardetect" />
       <Box ref={containerRef} style={{ width: '100vw', height: '100vh' }} />
       {currentStep < 4 && (
-        <Footer>
-          <Drawer open={true}>
-            {currentStep === 1 ? (
-              <IndicatorInfoList
-                title="Namiřte na přední stranu modemu."
-                subtitle="Kontrolky znamenají toto:"
-                list={['Napájení', 'DSL', 'Internet', 'Lan 1-4', 'Wi-Fi 2.5 Ghz', 'Wi-Fi 5Ghz']}
-              />
-            ) : null}
-            {currentStep === 0 ? (
-              <IndicatorInfoList
-                title="Namiřte na přední stranu modemu."
-                subtitle="Kontrolky znamenají toto:"
-                list={[
-                  'Napájení',
-                  'DSL',
-                  'Internet',
-                  'Lan 1-4',
-                  'Wi-Fi 2.5 Ghz',
-                  'Wi-Fi 5Ghz',
-                  'Internet',
-                  'Lan 1-4',
-                  'Wi-Fi 2.5 Ghz',
-                  'Wi-Fi 5Ghz',
-                ]}
-              />
-            ) : null}
-            <Button onClick={handlePreviousClick} variant="outlined">
-              Zpět
-            </Button>
-            <Button onClick={handleNextClick} variant="contained">
-              Pokračovat
-            </Button>
-          </Drawer>
-        </Footer>
+        <Drawer open={true}>
+          {currentStep === 1 ? (
+            <IndicatorInfoList
+              title="Namiřte na přední stranu modemu."
+              subtitle="Kontrolky znamenají toto:"
+              list={['Napájení', 'DSL', 'Internet', 'Lan 1-4', 'Wi-Fi 2.5 Ghz', 'Wi-Fi 5Ghz']}
+            />
+          ) : null}
+          {currentStep === 0 ? (
+            <IndicatorInfoList
+              title="Namiřte na přední stranu modemu."
+              subtitle="Kontrolky znamenají toto:"
+              list={[
+                'Napájení',
+                'DSL',
+                'Internet',
+                'Lan 1-4',
+                'Wi-Fi 2.5 Ghz',
+                'Wi-Fi 5Ghz',
+                'Internet',
+                'Lan 1-4',
+                'Wi-Fi 2.5 Ghz',
+                'Wi-Fi 5Ghz',
+              ]}
+            />
+          ) : null}
+          <Button onClick={handlePreviousClick} variant="outlined">
+            Zpět
+          </Button>
+          <Button onClick={handleNextClick} variant="contained">
+            Pokračovat
+          </Button>
+        </Drawer>
       )}
     </Box>
   );
