75ffa862893f1b64d9f229be3af0f620a72909c7
Eliáš Bauer <zenbauhaus@gmail.com>
2024-06-14 09:01:40 +0200

    Update ModelationAiFrontPage.tsx
    old logic readd

--- src/pages/Modelation/ModelationAiFrontPage.tsx
+++ src/pages/Modelation/ModelationAiFrontPage.tsx
@@ -10,10 +10,95 @@ type Props = {
   handleExecute: () => void;
 };
 
+
 export const ModelationAiFrontPage: FC<Props> = ({ detections, handleExecute }) => {
   const { redirectToStep, redirectToPage } = useModelationRouter();
   const [buttonText, setButtonText] = useState('Zkontrolovat');
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  
+  
+  const filterByLabelIncludes = (filterLabel) =>
+    posLabels.filter(({ label }) => label.includes(filterLabel));
+  
+  const filterByLabel = (filterLabel) =>
+    posLabels.filter(({ label }) => label === filterLabel);
+
+  // There are multiple port and ind classes, we want to count them all
+  const portCount = filterByLabelIncludes("port").length;
+  const indCount = filterByLabelIncludes("ind").length;
+
+  const lightoffCount = filterByLabel("lightoff").length;
+  const lightonCount = filterByLabel("lightg").length;
+  const lights = filterByLabelIncludes("light");
+
+  const portpowExists = filterByLabel("portpow");
+  const portdslExists = filterByLabel("portdsl");
+  const portwanExists = filterByLabel("portwan");
+
+  const cabpowExists = filterByLabel("cabpow");
+  const cabdslExists = filterByLabel("cabdsl");
+  const cabwanExists = filterByLabel("cabwan");
+
+  
+  const data = {
+    lightoffCount,
+    portCount,
+    indCount,
+    lightonCount,
+    lights,
+    portdslExists,
+    cabdslExists,
+    cabwanExists,
+    portpowExists,
+    portwanExists,
+    cabpowExists,
+  };
+
+  console.table(data);
+
+const processDetections = (detections) => {
+  const posLabels = detections.map((det) => {
+    const label = labels[det[5]];
+    const score = (det[4] * 100).toFixed(2);
+    const pos = parseInt(det[0]);
+    return {
+      xPosition: pos,
+      label: label,
+    };
+  });
+
+  posLabels.sort((a, b) => a.xPosition - b.xPosition);
+  console.table(posLabels);
+  return posLabels;
+};
+
+
+
+
+const processFrontSide = ({
+  lightoffCount,
+  portCount,
+  lightonCount,
+  lights,
+}) => {
+  console.log("front");
+  if (lights.length === 6) {
+    console.log(lights);
+  } else {
+    if (lightoffCount >= 5) {
+      console.log("zapn");
+    }
+
+    if (portCount >= 4) {
+      console.log("turn around");
+    }
+
+     if (lightonCount >= 4) {
+    console.log("Correct");
+  }
+};
+
+
 
   const handleButtonClick = () => {
     setIsButtonDisabled(true);
@@ -32,7 +117,7 @@ export const ModelationAiFrontPage: FC<Props> = ({ detections, handleExecute }) 
 
   const processDetections = () => {
     console.log(detections);
-    return [true, true, true, true, true, true];
+    return [false, false, false, false, false, false];
   };
 
 
@@ -67,4 +152,4 @@ export const ModelationAiFrontPage: FC<Props> = ({ detections, handleExecute }) 
       </Drawer>
     </Box>
   );
-};
+}};
