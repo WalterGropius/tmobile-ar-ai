893c939153407bb8184db8375a85094d25a3884a
Eliáš Bauer <zenbauhaus@gmail.com>
2024-06-21 18:41:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '192.png',
+            src: '192.jpg',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '512.png',
+            src: '512.jpg',
             sizes: '512x512',
             type: 'image/png',
           },
