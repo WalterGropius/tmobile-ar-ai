68e26d97a25f5a7e06595f84f0a1deddd6c77115
Eliáš Bauer <zenbauhaus@gmail.com>
2024-07-03 09:04:26 +0200

    Update LightIndicator.tsx
    always 6

--- src/ui/LightIndicator.tsx
+++ src/ui/LightIndicator.tsx
@@ -7,10 +7,14 @@ type Props = {
   statusList: boolean[];
 };
 
-export const LightIndicator: FC<Props> = ({ statusList }) => (
-  <Box sx={{ display: 'flex',justifyContent:'space-between', width: '100%',marginBottom:'38px' }}>
-    {statusList.map((status, key) => (
-      <Circle key={key} color={status ? Color.green : Color.black} />
-    ))}
-  </Box>
-);
+export const LightIndicator: FC<Props> = ({ statusList }) => {
+  const displayList = statusList.length === 6 ? statusList : Array(6).fill(false);
+
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '38px' }}>
+      {displayList.map((status, key) => (
+        <Circle key={key} color={status ? Color.green : Color.black} />
+      ))}
+    </Box>
+  );
+};
