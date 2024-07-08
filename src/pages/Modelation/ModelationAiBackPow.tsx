96734d07442f599d137c46961144edb5dd8eff98
Eliáš Bauer <zenbauhaus@gmail.com>
2024-07-08 17:46:54 +0200

    revert powControl

--- src/pages/Modelation/ModelationAiBackPow.tsx
+++ src/pages/Modelation/ModelationAiBackPow.tsx
@@ -1,65 +1,23 @@
+
 import { useModelationRouter } from '../../hooks/useModelationRouter';
 import { StatusBanner } from '../../ui/StatusBanner';
 import { Box, Button, Typography } from '@mui/material';
 import { Drawer } from '../../ui/Drawer';
-import { Notification } from '../../ui/Notification';
-import { useState, useEffect, useCallback } from 'react';
-import useBackPowDetect from '../../hooks/useBackPowDetect';
-import { FC } from 'react';
-import { ModelationAiBackPowPageProps } from '../../types/modelation';
+import { useState, useEffect } from 'react';
 
-export const ModelationAiBackPowPage: FC<ModelationAiBackPowPageProps> = ({ labeledDetections, handleExecute }) => {
+export const ModelationAiBackCabPage = () => {
   const { redirectToStep } = useModelationRouter();
-  const [buttonState, setButtonState] = useState<'init' | 'loading' | 'done'>('init');
-  const [buttonClickCount, setButtonClickCount] = useState(0);
-  const cableStatus = useBackPowDetect(labeledDetections);
 
-  const executeDetect = useCallback(() => {
-    setButtonState('loading');
+  const [buttonText, setButtonText] = useState('Zkontrolovat');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const handleExecute = () => {
+    setIsButtonDisabled(true);
     setTimeout(() => {
-      handleExecute();
-      setButtonState('done');
-    }, 1000);
-  }, [handleExecute]);
-
-  const handleButtonClick = useCallback(() => {
-    setButtonClickCount(prevCount => prevCount + 1);
-    executeDetect();
-  }, [executeDetect]);
-
-
-  useEffect(() => {
-    if (buttonState === 'done') {
-      setButtonState('init');
-    }
-  }, [buttonState]);
-
-  useEffect(() => {
-    if (buttonClickCount >= 5 || cableStatus === 'correct') {
-      setTimeout(() => {
-        redirectToStep('powButt');
-      }, 1000);
-    }
-  }, [buttonClickCount, cableStatus, redirectToStep]);
-
-  const renderCableStatus = () => {
-    switch (cableStatus) {
-      case 'correct':
-        return <Typography variant="h2">Spravné zapojení ✓</Typography>;
-      case 'error':
-        return (
-          <Notification
-            title="Nespravné zapojení"
-            message="Zkontrolujte prosím připojení napájecího kabelu a zkuste to znovu."
-          />
-        );
-      case 'flip':
-        return <Notification title="Otočte modem" message="Je potřeba zkontrolovat napájecí kabel." />;
-      case 'no-cabPow':
-        return <Notification title="Chyba Analýzy" message="Kabel nenalezen." />;
-      default:
-        return <Typography variant="h4">Probihá AI kontrola...</Typography>;
-    }
+      setButtonText('Pokračovat');
+      redirectToStep('powerAnim');
+      setIsButtonDisabled(false);
+    }, 5000);
   };
 
   return (
@@ -69,27 +27,22 @@ export const ModelationAiBackPowPage: FC<ModelationAiBackPowPageProps> = ({ labe
       </Box>
       <Drawer open={true}>
         <Box sx={{ my: 0 }}>
-          <Typography variant="h2">Namiřte na zadní část modemu</Typography>
-          <Typography sx={{ my: '24px' }} variant="h4">
-            Výsledný stav (proces může trvat až 2 minuty)
-          </Typography>
-
-          {renderCableStatus()}
-
+        <Typography variant="h2">Namiřte na zadní část modemu</Typography>
+         <Typography sx={{my:'24px'}} variant="h4">Výsledný stav (proces může trvat až 2 minuty)</Typography>
           <Box sx={{ display: 'flex', mt: 1 }}>
             <Box sx={{ width: '40%', pr: 1 }}>
-              <Button variant="outlined" fullWidth onClick={() => redirectToStep('aiBackCab')}>
+              <Button variant="outlined" fullWidth onClick={() => redirectToStep('cableAnim')}>
                 Zpět
               </Button>
             </Box>
             <Box sx={{ width: '100%' }}>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={handleButtonClick}
-                disabled={buttonState === 'loading'}
+              <Button 
+                variant="contained" 
+                fullWidth 
+                onClick={handleExecute} 
+                disabled={isButtonDisabled}
               >
-                {buttonState === 'loading' ? 'Kontrola' : 'Zkontrolovat'}
+                {buttonText}
               </Button>
             </Box>
           </Box>
