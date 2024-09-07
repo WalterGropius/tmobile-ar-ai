0c6955daa1ecb81da1cce4c3106e658aa6eb2968
Eliáš Bauer <zenbauhaus@gmail.com>
2024-09-07 04:45:12 +0200

    Update useModelationRouter.ts

--- src/hooks/useModelationRouter.ts
+++ src/hooks/useModelationRouter.ts
@@ -1,10 +1,8 @@
-import { resolveConnectionType } from '../core/connection/resolveConnectionType';
 import { useSearchParams } from 'react-router-dom';
 import { Step } from '../types/modelation';
 
 export const useModelationRouter = () => {
   const [searchParams] = useSearchParams();
-  const connectionType = resolveConnectionType(searchParams.get('connection') || '');
 
   const redirect = (url: string, refreshAfterRedirect: boolean) => {
     window.location.href = url;
@@ -14,10 +12,8 @@ export const useModelationRouter = () => {
   };
 
   return {
-    connectionType,
     step: (searchParams.get('step') || '') as Step,
     redirectToPage: (page: string, refresh = false) => redirect(`/#/${page}`, refresh),
-    redirectToStep: (step: Step, refresh = false) =>
-      redirect(`/#/ar-viewer?connection=${connectionType}&step=${step}`, refresh),
+    redirectToStep: (step: Step, refresh = false) => redirect(`/#/ar-viewer?step=${step}`, refresh),
   };
 };
